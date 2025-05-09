a882c02a81a1262690d0a17638713c5e9f21c73f
Leo Du <145289436+leo-du-03@users.noreply.github.com>
2025-05-09 15:15:42 -0400

    Add files via upload

---
+++ gym_pybullet_drones/envs/PathAviary.py
@@ -0,0 +1,155 @@
+import numpy as np
+import random
+
+from gym_pybullet_drones.envs.BaseRLAviary import BaseRLAviary
+from gym_pybullet_drones.utils.enums import DroneModel, Physics, ActionType, ObservationType
+
+class PathAviary(BaseRLAviary):
+    """Single agent RL problem: hover at position."""
+
+    ################################################################################
+    
+    def __init__(self,
+                 drone_model: DroneModel=DroneModel.CF2X,
+                 initial_xyzs=None,
+                 initial_rpys=None,
+                 physics: Physics=Physics.PYB,
+                 pyb_freq: int = 240,
+                 ctrl_freq: int = 30,
+                 gui=False,
+                 record=False,
+                 obs: ObservationType=ObservationType.KIN,
+                 act: ActionType=ActionType.RPM
+                 ):
+        """Initialization of a single agent RL environment.
+
+        Using the generic single agent RL superclass.
+
+        Parameters
+        ----------
+        drone_model : DroneModel, optional
+            The desired drone type (detailed in an .urdf file in folder `assets`).
+        initial_xyzs: ndarray | None, optional
+            (NUM_DRONES, 3)-shaped array containing the initial XYZ position of the drones.
+        initial_rpys: ndarray | None, optional
+            (NUM_DRONES, 3)-shaped array containing the initial orientations of the drones (in radians).
+        physics : Physics, optional
+            The desired implementation of PyBullet physics/custom dynamics.
+        pyb_freq : int, optional
+            The frequency at which PyBullet steps (a multiple of ctrl_freq).
+        ctrl_freq : int, optional
+            The frequency at which the environment steps.
+        gui : bool, optional
+            Whether to use PyBullet's GUI.
+        record : bool, optional
+            Whether to save a video of the simulation.
+        obs : ObservationType, optional
+            The type of observation space (kinematic information or vision)
+        act : ActionType, optional
+            The type of action space (1 or 3D; RPMS, thurst and torques, or waypoint with PID control)
+
+        """
+        self.EPISODE_LEN_SEC = 8
+        super().__init__(drone_model=drone_model,
+                         num_drones=1,
+                         initial_xyzs=initial_xyzs,
+                         initial_rpys=initial_rpys,
+                         physics=physics,
+                         pyb_freq=pyb_freq,
+                         ctrl_freq=ctrl_freq,
+                         gui=gui,
+                         record=record,
+                         obs=obs,
+                         act=act
+                         )
+
+    ################################################################################
+
+    def reset(self,
+              seed : int = None,
+              options : dict = None):
+        pos = self.pos
+        quat = self.quat
+        rpy = self.rpy
+        vel = self.vel
+        ang_v = self.ang_v
+        initial_obs, initial_info = super().reset()
+        self.pos = pos
+        self.quat = quat
+        self.rpy = rpy
+        self.vel = vel
+        self.ang_v = ang_v
+        self.TARGET_POS = np.array([random.uniform(-2.5, 2.5), random.uniform(2.5,2.5), random.uniform(0.5,3)])
+        initial_obs = self._computeObs()
+        return initial_obs, initial_info
+
+    ################################################################################
+    
+    def _computeReward(self):
+        """Computes the current reward value.
+
+        Returns
+        -------
+        float
+            The reward.
+
+        """
+        state = self._getDroneStateVector(0)
+        # velocity is stored in state[10:13], give reward when < 0.1
+        ret = 0
+        if np.linalg.norm(state[10:13]) < 1:
+            ret = max(0, 2 - np.linalg.norm(self.TARGET_POS-state[0:3])**4)
+        return ret
+
+    ################################################################################
+    
+    def _computeTerminated(self):
+        """Computes the current done value.
+
+        Returns
+        -------
+        bool
+            Whether the current episode is done.
+
+        """
+        state = self._getDroneStateVector(0)
+        if np.linalg.norm(self.TARGET_POS-state[0:3]) < .0001 and np.linalg.norm(state[10:13]) < 1:
+            return True
+        else:
+            return False
+        
+    ################################################################################
+    
+    def _computeTruncated(self):
+        """Computes the current truncated value.
+
+        Returns
+        -------
+        bool
+            Whether the current episode timed out.
+
+        """
+        state = self._getDroneStateVector(0)
+        if (abs(state[0]) > 2.5 or abs(state[1]) > 2.5 or state[2] > 3 # Truncate when the drone is too far away
+             or abs(state[7]) > .4 or abs(state[8]) > .4 # Truncate when the drone is too tilted
+        ):
+            return True
+        if self.step_counter/self.PYB_FREQ > self.EPISODE_LEN_SEC:
+            return True
+        else:
+            return False
+
+    ################################################################################
+    
+    def _computeInfo(self):
+        """Computes the current info dict(s).
+
+        Unused.
+
+        Returns
+        -------
+        dict[str, int]
+            Dummy value.
+
+        """
+        return {"answer": 42} #### Calculated by the Deep Thought supercomputer in 7.5M years
